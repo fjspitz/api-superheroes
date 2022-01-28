cd99f40ec572182e2676f17681ca241ddf944f00
Fernando J. Spitz <fernando.spitz@localpayment.com>
2022-01-28 12:03:16 -0300

    Initial commit

---
+++ src/main/java/ar/com/fjs/api/superheroes/ApiSuperheroesApplication.java
@@ -0,0 +1,13 @@
+package ar.com.fjs.api.superheroes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSuperheroesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSuperheroesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/com/fjs/api/superheroes/ApiSuperheroesApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.fjs.api.superheroes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSuperheroesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
